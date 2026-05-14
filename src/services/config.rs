f7c74070f219f7deb7ed49b7a899ca971f8a293c
XMrgao <1099607871@qq.com>
2026-05-14 14:46:36 +0800

    Update config.rs

--- src/services/config.rs
+++ src/services/config.rs
@@ -401,7 +401,8 @@ fn get_config_template() -> serde_json::Value {
             "rules": [{"rule_set": ["chinasite"], "action": "route", "server": "local"}]
         },
         "inbounds": [
-            {"type": "tun", "tag": "tun-in", "interface_name": "sing-tun", "address": ["172.18.0.1/30"], "mtu": 9000, "auto_route": true, "strict_route": true, "auto_redirect": true}
+            {"type": "tun", "tag": "tun-in", "interface_name": "sing-tun", "address": ["172.18.0.1/30"], "mtu": 9000, "auto_route": true, "strict_route": true, "auto_redirect": true},
+            {"type": "http","tag": "http-in","listen": "::", "listen_port": 7890,"sniff": true,"set_system_proxy": false}
         ],
         "outbounds": [
             {"type": "selector", "tag": "proxy", "outbounds": []},
